8b0daf850bdd68d1d020f925ade6942546fbecd3
Damon Morris <d.j.morris@sheffield.ac.uk>
2024-03-12 11:33:04 +0000

    add Holly's code for the labour market data analysis

--- 2_labour_market/R_scripts/Weeks Worked by Health Condition/2) Adding 0s to Months worked for non employed.R
+++
@@ -1,8 +0,0 @@
-#if emplx = 0, then wavexmonths = 0. 
-
-monthsworked2 <- monthsworked 
-
-monthsworked2 <- monthsworked2 %>% mutate(wave2months = ifelse(Empl2 < 0.5, 0, wave2months))
-monthsworked2 <- monthsworked2 %>% mutate(wave3months = ifelse(Empl3 < 0.5, 0, wave3months))
-monthsworked2 <- monthsworked2 %>% mutate(wave4months = ifelse(Empl4 < 0.5, 0, wave4months))
-monthsworked2 <- monthsworked2 %>% mutate(wave5months = ifelse(Empl5 < 0.5, 0, wave5months))
