72eea61142fdb5de38e36dcd0509d2c198300cf2
Damon Morris <d.j.morris@sheffield.ac.uk>
2024-04-11 10:36:26 +0100

    update filepaths to UKMOD model folders

--- 5_ukmod_data_inputs/R_scripts/5d_UKMOD_model_file_paths.R
+++ 5_ukmod_data_inputs/R_scripts/5d_UKMOD_model_file_paths.R
@@ -1,18 +1,23 @@
 ## files for UKMOD to be able to read the input
 
-path <- "X:/HAR_PR/PR/IPS_beyond_SMI_NIHR202996/General/UKMOD/Scenarios/"
+### SPECIFY UKMOD VERSION HERE:
+ukmod_version <- "B1.09"
 
-path_msk_oow       <- "UKMOD_PUBLIC_B1.09_MSK_OOW/"
-path_msk_iw        <- "UKMOD_PUBLIC_B1.09_MSK_IW/"
-path_msk_iw_1yr    <- "UKMOD_PUBLIC_B1.09_MSK_IW_1yr/"
-path_msk_iw_1yr_n  <- "UKMOD_PUBLIC_B1.09_MSK_IW_1yr_nonemp/"
+### DEFINE FILE PATHS
 
-path_mh_oow       <- "UKMOD_PUBLIC_B1.09_MH_OOW/"
-path_mh_iw        <- "UKMOD_PUBLIC_B1.09_MH_IW/"
-path_mh_iw_1yr    <- "UKMOD_PUBLIC_B1.09_MH_IW_1yr/"
-path_mh_iw_1yr_n  <- "UKMOD_PUBLIC_B1.09_MH_IW_1yr_nonemp/"
+path <- paste0("X:/HAR_PR/PR/IPS_beyond_SMI_NIHR202996/General/UKMOD/Scenarios/",ukmod_version,"/UKMOD_PUBLIC_")
 
-path_msk_mh_oow       <- "UKMOD_PUBLIC_B1.09_BOTH_OOW/"
-path_msk_mh_iw        <- "UKMOD_PUBLIC_B1.09_BOTH_IW/"
-path_msk_mh_iw_1yr    <- "UKMOD_PUBLIC_B1.09_BOTH_IW_1yr/"
-path_msk_mh_iw_1yr_n  <- "UKMOD_PUBLIC_B1.09_BOTH_IW_1yr_nonemp/"
+path_msk_oow       <- paste0(ukmod_version,"_MSK_OOW/")
+path_msk_iw        <- paste0(ukmod_version,"_MSK_IW/")
+path_msk_iw_1yr    <- paste0(ukmod_version,"_MSK_IW_1yr/")
+path_msk_iw_1yr_n  <- paste0(ukmod_version,"_MSK_IW_1yr_nonemp/")
+
+path_mh_oow       <- paste0(ukmod_version,"_MH_OOW/")
+path_mh_iw        <- paste0(ukmod_version,"_MH_IW/")
+path_mh_iw_1yr    <- paste0(ukmod_version,"_MH_IW_1yr/")
+path_mh_iw_1yr_n  <- paste0(ukmod_version,"_MH_IW_1yr_nonemp/")
+
+path_msk_mh_oow       <- paste0(ukmod_version,"_BOTH_OOW/")
+path_msk_mh_iw        <- paste0(ukmod_version,"_BOTH_IW/")
+path_msk_mh_iw_1yr    <- paste0(ukmod_version,"_BOTH_IW_1yr/")
+path_msk_mh_iw_1yr_n  <- paste0(ukmod_version,"_BOTH_IW_1yr_nonemp/")
